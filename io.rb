35d1be058aff32e6243570fed6350aec07bc785a
James Cook <jamesmarkcook@gmail.com>
2013-06-08 15:20:22 -0400

    Classes are now setup and functioning properly

--- io.rb
+++ io.rb
@@ -1,21 +1,74 @@
-require_relative 'irc_connection'
+require 'socket'
 require_relative 'logic'
+require_relative 'irc_connection'
+require 'pry'
 
-Connection.new
 
 class IO
 
-	@input = Connection.input
-	@output = Connection.output
+	# Create new instance of Connection
+	@c = Connection.new
+
+	# Opens a new connection to IRC with the correct port and server.
+	@irc = TCPSocket.open(@c.server, @c.port)
+
+	# white noise... 
+	# print("addr: ", s.addr.join(":"), "\n")
+	# print("peer: ", s.peeraddr.join(":"), "\n")
+
+	# Sets up a new IRC session using the settings from he Connection class.
+	@irc.puts "USER #{@c.user} 0 * #{@c.user}"
+	@irc.puts "NICK #{@c.nick}"
+	@irc.puts "JOIN #{@c.channel}"
+	@irc.puts "PRIVMSG #{@c.channel} :Awright geezzaaa! Hello from Cockneybot. Sorted mate."
 
 
-	if @input.include? "hello"
-		Connection.connection.puts @output + "Hello!!!"
-		puts "Testtttttttt"
+
+	def self.output(message)
+		@irc.puts "PRIVMSG #{@c.channel} :" + message.to_s
 	end
 
 
-end
+	# Loop until the connection closes. Enclose all IO in here.
+	until @irc.eof? do
+		@input = @irc.gets.to_s
+	  	puts @input
+
+
+	  	if @input.include? "Hello"
+	  		self.output("Hey!")
+	  	end
+
+
+
+
+
+
+
+
+
+
+	end
+
+
+
+
+
+
+
+	# def self.output(message)
+	# 	irc_connection.output(message)
+	# end
+
+
+	# while true
+	# 	self.output("Testing 1..2..1..2")
+	# 	puts "Testing!!!!!"
+	# 	sleep 5
+	# end
+
+
+end	
 
 	
 
